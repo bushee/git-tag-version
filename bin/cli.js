0f21378423ab559d4739ad3a35e78488365b7e59
Krzysztof "Bushee" Nowaczyk <bushee01@gmail.com>
2018-10-09 10:09:58 +0200

    cli

---
+++ bin/cli.js
@@ -0,0 +1,9 @@
+var gitTagVersion = require('../src/git-tag-version');
+
+var options = {
+    uniqueSnapshot: process.argv.some(function (arg) {
+        return arg === '--uniqueSnapshot' || arg === '--unique-snapshot';
+    })
+};
+
+console.log(gitTagVersion(options));
